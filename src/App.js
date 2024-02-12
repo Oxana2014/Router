f795a2eaacc4d6d4b3a5e04f9fab91f2f476d1cb
Oksana Kormilina <keysb@mail.ru>
2024-02-12 17:27:07 +0400

    Add dynamic routs

--- src/App.js
+++ src/App.js
@@ -6,18 +6,19 @@ import {
 } from "react-router-dom";
 import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
-
+import ProductDetailPage from "./pages/ProductDetail";
 import RootLayout from "./pages/Root";
 import ErrorPage from "./pages/Error";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/tt",
     element: <RootLayout />,
     errorElement: <ErrorPage />,
     children: [
       { path: "/", element: <HomePage /> },
       { path: "/products", element: <ProductsPage /> },
+      {path: "/products/:productId", element: <ProductDetailPage />}
     ],
   },
 ]);
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,18 @@
-import {Link} from 'react-router-dom'
+import { Link, useNavigate } from "react-router-dom";
 
 export default function HomePage() {
-    return <>
-    <h1>My Home Page</h1>
-    <p>Go to <Link to="/products">the list of products</Link></p>
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    navigate("/products");
+  }
+  return (
+    <>
+      <h1>My Home Page</h1>
+      <p>
+        Go to <Link to="/products">the list of products</Link>
+      </p>
+      <button onClick={navigateHandler}>Navigate to products</button>
     </>
-} 
+  );
+}
